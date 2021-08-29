317c3c5ba17fddd3ac79699b255481b5a9bd5202
DJ Taylor <dtaylor88@gmail.com>
2021-08-28 23:00:49 -0700

    commit tree 8edf660943a135a11e1fb5286e229b506a38e5a3

--- main.js
+++ main.js
@@ -2,7 +2,45 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+document.getElementById('modal').className = 'hidden';
+
+
+const postCollection = document.querySelectorAll('.media-post');
+postCollection.forEach(post => {
+  post.addEventListener('click', function(event) {
+    let likeButtonIsPressed = event.target.className === "like-glyph"
+    if (likeButtonIsPressed) {
+      mimicServerCall()
+      .then(function(response){
+        let heart = event.target;
+        heart.innerHTML = FULL_HEART;
+        heart.className = 'activated-heart';
+        heart.style.cursor = 'pointer';
+        console.log(response)
+      })
+      .catch(function(e) {
+        let modal = document.getElementById('modal');
+        modal.className = '' 
+        let message = document.getElementById('modal-message')
+        message.innerHTML = e;
+        setTimeout(function() {
+          modal.className = 'hidden' 
+        }, 3000);
+      })
+    }
+  })
+})
+
+postCollection.forEach(post => {
+  post.addEventListener('click', function(event) {
+    let likeButtonIsPressed = event.target.className === 'activated-heart';
+    if (likeButtonIsPressed) {
+      event.target.className = 'like-glyph';
+      event.target.innerHTML = EMPTY_HEART;
+    }
+  })
+})
+
 
 
 
